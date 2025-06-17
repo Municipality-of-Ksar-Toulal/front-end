9e2c083c166b1fe40b5c32df7d149412209c0c56
SN-101 <samirnassiri67@gmail.con>
2025-06-17 16:35:23 +0100

    add imgs for slide and langs

--- main.js
+++ main.js
@@ -1,15 +1,35 @@
 const translations = {
     ar: {
         bigTitle: "بلدية قصر تولال",
+        navbar1:"الرئيسية",
+        navbar2:"من نحن",
+        navbar3:"خدمات",
+        navbar4:"أخبار",
+        navbar5:"تواصل معنا",
     },
     fr: {
         bigTitle: "Commune de Kasr Toullal",
+        navbar1:"Accueil",
+        navbar2:"Sommes-nous",
+        navbar3:"Services",
+        navbar4:"Nouvelles",
+        navbar5:"Contactez-nous",
     },
     en: {
         bigTitle: "Municipality of Kasr Toullal",
+        navbar1:"Home",
+        navbar2:"About us",
+        navbar3:"Services",
+        navbar4:"Nwes",
+        navbar5:"Contact us",
     },
     am: {
         bigTitle: "ⵜⴰⵖⵉⵡⴰⵏⵜ ⵏ ⵅⵚⴰⵔ ⵜⵓⵍⴰⵍ",
+        navbar1:"ⴰⵅⴰⵎ",
+        navbar2:"ⴰⵏⵡⴰ ⵉ ⵏⴻⵍⵍⴰ",
+        navbar3:"ⵉⵎⴻⵥⵍⴰ",
+        navbar4:"ⵉⵎⴰⵢⵏⵓⵜⴻⵏ",
+        navbar5:"ⵣⵣⵓ ⵖⵓⵔⵏⴻⵖ",
     }
 };
 
@@ -25,12 +45,12 @@ function setLanguage(lang) {
     });
   }
 
-// const btns=document.querySelectorAll(".btn");
-// btns.forEach(btn=>{
-//     btn.addEventListener("click", function(){
-//         document.querySelector(".btn.active")?.classList.remove("active");
-//         this.classList.add("active");
-//     });
-// });
+const btns=document.querySelectorAll(".btn");
+btns.forEach(btn=>{
+    btn.addEventListener("click", function(){
+        document.querySelector(".btn.active")?.classList.remove("active");
+        this.classList.add("active");
+    });
+});
 
 setLanguage("ar");
